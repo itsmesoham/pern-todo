f56a673b138159ba4c7df1cc2d9a35d44e764e44
itsmesoham <sohampoptani7@gmail.com>
2025-10-07 15:37:46 +0530

    commit - 10/7/25 at 3:37 pm

--- client/src/components/InputTodo.jsx
+++ client/src/components/InputTodo.jsx
@@ -41,7 +41,7 @@ const InputTodo = () => {
     return (
         <Fragment>
             <h1 className='text-center mt-5'>PERN Todo List</h1>
-            <form className='d-flex mt-5' onSubmit={onSubmitForm}>
+            <form className='d-flex mt-4' onSubmit={onSubmitForm}>
                 <input
                     type='text'
                     className='form-control'
